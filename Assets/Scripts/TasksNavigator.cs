b2a058ecec652b5c575e6171ad9c19eb7be3cff9
Sascha Jentsch <dev@szyx.dev>
2023-01-10 13:51:00 +0100

    Templates
    Add circle template generation

--- Assets/Scripts/TasksNavigator.cs
+++ Assets/Scripts/TasksNavigator.cs
@@ -7,13 +7,16 @@ public class TasksNavigator : MonoBehaviour
 
     public GameObject MessageBox;
     public GameObject NextButton;
+    public GameObject TemplateBase;
 
     private UnityEngine.UI.Text _text;
     private int _counter;
+    private TemplateRenderer _templateRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
+        _templateRenderer = new TemplateRenderer();
         _text = MessageBox.GetComponent<UnityEngine.UI.Text>();
         _text.text = Messages.Intro;
         _counter = 0;
@@ -35,10 +38,14 @@ public class TasksNavigator : MonoBehaviour
                 break;
             case 3:
                 _text.text = Messages.TasksAboutToStart;
-                // TODO: Spawn Tasks next
+                _templateRenderer.RenderCircle(TemplateBase, TemplateRenderer.Rotation.Horizontal);
+                break;
+            case 4:
+                _templateRenderer.RenderCircle(TemplateBase, TemplateRenderer.Rotation.Vertical);
                 break;
             default:
                 _counter = 0;
+                _templateRenderer.ResetLine();
                 break;
         }
         _counter++;
---
+++ Assets/Scripts/TemplateRenderer.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TemplateRenderer
+{
+
+    public enum Rotation
+    {
+        Horizontal,
+        Vertical,
+        Angled
+    }
+
+    private LineRenderer _line;
+    private Vector3 _pos;
+
+    public void RenderCircle(GameObject container, Rotation rotation)
+    {
+
+        int segments = 360;
+        float radius = 0.1f;
+        int pointCount = segments + 1;
+        _line = container.GetComponent<LineRenderer>();
+        _line.positionCount = pointCount;
+        _line.useWorldSpace = true;
+        _line.startWidth = 0.028f;
+        _line.endWidth = 0.028f;
+
+        Vector3[] points = new Vector3[pointCount];
+        _pos = new Vector3(container.transform.position.x, container.transform.position.y, container.transform.position.z);
+
+        for (int i = 0; i < pointCount; i++)
+        {
+            float rad = Mathf.Deg2Rad * (i * 360f / segments);
+            points[i] = calcPointBasedOnRotation(rotation, rad, radius);
+        }
+
+        _line.SetPositions(points);
+    }
+
+    private Vector3 calcPointBasedOnRotation(Rotation rotation, float rad, float radius)
+    {
+        switch (rotation)
+        {
+            case Rotation.Horizontal:
+                return new Vector3(_pos.x + Mathf.Cos(rad) * radius,
+                                    _pos.y + 0,
+                                    _pos.z + Mathf.Sin(rad) * radius);
+            case Rotation.Vertical:
+                return new Vector3(_pos.x + 0,
+                                    _pos.y + Mathf.Cos(rad) * radius,
+                                    _pos.z + Mathf.Sin(rad) * radius);
+            case Rotation.Angled:
+                // TODO: Implement angle circle
+            default:
+                return new Vector3(0f, 0f, 0f);
+        }
+    }
+
+    public void ResetLine()
+    {
+        _line.positionCount = 0;
+    }
+}
